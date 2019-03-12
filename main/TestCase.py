93019334afe93411ac8cea3cee990c130b186b5e
npeersab <npeersab77@gmail.com>
2019-03-12 18:24:16 +0530

    create Test Case class

---
+++ main/TestCase.py
@@ -0,0 +1,22 @@
+'''
+Created on Mar 12, 2019
+
+@author: Noor Peersab
+'''
+
+
+class TestCase(object):
+    '''
+    Test Case
+    '''
+
+    def __init__(self, test_case_name, source_connection, source_query, target_connection, target_query):
+        '''
+        Constructor
+        '''
+        
+    def execute(self):
+        '''
+        Execute the Test Case
+        '''
+        pass
