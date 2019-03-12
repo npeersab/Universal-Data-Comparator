5f7c33d661844ee61907fc93a9ff4cfe36dfb6bb
npeersab <npeersab77@gmail.com>
2019-03-12 21:11:10 +0530

    reformat code

--- main/Connection.py
+++ main/Connection.py
@@ -1,23 +1,22 @@
-'''
+"""
 Created on Mar 11, 2019
 
 @author: Noor Peersab
-'''
+"""
+
 
 class Connection(object):
-    '''
+    """
     Connection to the database
-    '''
+    """
 
-
-    def __init__(self, connection_name, database, host, dbname, port):
-        '''
+    def __init__(self, connection_name, database, host, db_name, port):
+        """
         constructor to create new Connection
-        '''
+        """
         
         self.name = connection_name
         self.database = database
         self.host = host
-        self.dbname = dbname,
-        self.port = port    
-    
+        self.db_name = db_name
+        self.port = port
--- main/TestCase.py
+++ main/TestCase.py
@@ -1,22 +1,27 @@
-'''
+"""
 Created on Mar 12, 2019
 
 @author: Noor Peersab
-'''
+"""
 
 
 class TestCase(object):
-    '''
+    """
     Test Case
-    '''
+    """
 
-    def __init__(self, test_case_name, source_connection, source_query, target_connection, target_query):
-        '''
+    def __init__(self, *, test_case_name, source_connection, source_query, target_connection, target_query):
+        """
         Constructor
-        '''
-        
+        """
+        self.test_case_name = test_case_name
+        self.source_connection = source_connection
+        self.source_query = source_query
+        self.target_connection = target_connection
+        self.target_query = target_query
+
     def execute(self):
-        '''
+        """
         Execute the Test Case
-        '''
+        """
         pass
