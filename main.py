75736eef1f8736d440372d29eccfad4ebf050381
npeersab <npeersab77@gmail.com>
2019-04-08 22:29:37 +0530

    implement logger

--- main.py
+++ main.py
@@ -13,12 +13,17 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import datetime
+import logging
+import os
 import sys
 
 from PyQt5 import QtWidgets
 
 from dialog import WelcomeDialog
 from window import MainWindow
+
+logger = logging.getLogger()
 
 
 def main():
@@ -68,12 +73,33 @@ def start_main(project):
     main_window.show()
 
 
+def config_logger():
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging.DEBUG)
+
+    folder_name = 'logs'
+    os.makedirs(folder_name, exist_ok=True)
+    file_name = str(datetime.datetime.now()).replace(':', '-').replace('.', '-')
+    file_handler = logging.FileHandler('{0}/{1}.log'.format(folder_name, file_name), mode='w')
+    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
+    stream_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    root_logger.addHandler(stream_handler)
+    root_logger.addHandler(file_handler)
+
+
 if __name__ == '__main__':
+    config_logger()
+
+    logger.info('Starting Application...')
     app = QtWidgets.QApplication(sys.argv)
     main_window = None
     welcome_dialog = WelcomeDialog(start_main)
     welcome_dialog.show()
+    logger.info('Application Started')
     sys.exit(app.exec_())
-
-
-
