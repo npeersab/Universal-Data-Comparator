470d0cab4d46248c8bcfc27a8cd4dc78b79acd87
npeersab <npeersab77@gmail.com>
2019-04-27 13:17:36 +0530

    remove unused code

--- main.py
+++ main.py
@@ -26,47 +26,6 @@ from window import MainWindow
 logger = logging.getLogger()
 
 
-def main():
-
-    '''user = UserDetails(username='postgres', password='123')
-    connection = Connection(
-        name='Test', host='localhost', db_name='postgres', port='5432')
-    connection.connect(user)
-
-    source_query = 'select * from test2'
-    target_query = 'select * from test1'
-    test_case = TestCase(test_case_name='Test Case 1', source_connection=connection, source_query=source_query,
-                         target_connection=connection, target_query=target_query, max_mismatch_size=100000)
-
-    test_result = test_case.execute()
-    print('source mismatch length: {}'.format(len(test_result.source_mismatch)))
-    print('target mismatch length: {}'.format(len(test_result.target_mismatch)))
-
-    '''
-    '''
-
-    start = time.perf_counter()
-    connection.execute_query('select * from test1')
-    print('source time {} sec'.format(time.perf_counter() - start))
-    source = connection.get_result_generator()
-
-    connection2 = Connection(
-        connection_name='Test', database='postgres', host='localhost', db_name='postgres', port='5432')
-    connection2.connect(user)
-    start = time.perf_counter()
-    connection2.execute_query('select * from test2')
-    target = connection2.get_result_generator()
-    print('target time {} sec'.format(time.perf_counter() - start))
-    
-    start = time.perf_counter()
-    source_mis, target_mis = comparator.compare(source, target, max_mismatch_size=10000)
-    print('compare time {} sec'.format(time.perf_counter() - start))
-
-    print('source mismatch length: {}'.format(len(source_mis)))
-    print('target mismatch length: {}'.format(len(target_mis)))
-    pass'''
-
-
 def start_main(project):
     global main_window
     main_window = MainWindow(project)
